2264924877a125655a2c1fa380d6d8ed0af66eab
yazorinp <149029477+yazorinp@users.noreply.github.com>
2023-10-28 13:44:06 +0500

    MainApp

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,29 @@
-function Main() {
-    return (
-        <div className="Main">
-            Hello World!
-        </div>
-    );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+class MainApp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addNum = this.addNum.bind(this);
+        this.state = {num: 0};
+    }
+
+    addNum(){
+        this.setState({num: this.state.num + 1});
+    }
+
+    render() {
+        return (
+            <dev>
+                <button onClick={this.addNum}>
+                    {this.state.num}
+                </button>
+            </dev>
+        );
+    }
 }
 
-
-export default Main;
+export default MainApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Main from './Main.js';
+import MainApp from './Main.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Main />
-);
+root.render(<MainApp/>)
