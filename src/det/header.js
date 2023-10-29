095bb4e268156d41c49b768bb9a7036849d4e78a
yazorinp <149029477+yazorinp@users.noreply.github.com>
2023-10-29 09:16:05 +0500

    Add Logo

--- src/det/header.js
+++ src/det/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './header.css';
+import Logo from '../images/logo.png'
 
 
 class Header extends React.Component {
@@ -8,18 +10,51 @@ class Header extends React.Component {
     }
 
 
+    viewLogo() {
+        if (this.props['logo'] === undefined)
+            return <Link to='/'><img src={Logo} alt='Logo' width='256px'/></Link>;
+        else
+            return <Link to='/'>{this.props['logo']}</Link>
+    }
+
+
+    viewMems() {
+        return <p>Здесь скоро что-то будет</p>;
+    }
+
+
+    viewLogInOut() {
+        return (
+            <div>
+                <Link to='/enter'>Войти</Link> | <Link to='/register'>Зарегистрироваться</Link>
+            </div>
+        );
+    }
+
+
+    viewSection() {
+        let section = this.props['section'];
+        if (section === undefined)
+            section = [<p>Главная</p>, <p>Возможности</p>];
+
+        return (
+            <table className='section'>
+                {section.map(elem => <th>{elem}</th>)}
+            </table>
+        );
+    }
+
+
     render() {
         return (
-            <table>
-                <tr>
-                    <th>
-                        <Link to="/">Root</Link>
-                    </th>
-                    <th>
-                        <Link to="/about">About</Link>
-                    </th>
-                </tr>
-            </table>
+            <div>
+                <div className='header'>
+                    <div>{this.viewLogo()}</div>
+                    <div>{this.viewMems()}</div>
+                    <div>{this.viewLogInOut()}</div>
+                </div>
+                <div>{this.viewSection()}</div>
+            </div>
         );
     }
 }
